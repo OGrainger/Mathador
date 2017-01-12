a3cb9499f7cdd5b8b9afd7ffeb98614a9662de08
OGrainger <oscarvgrainger@gmail.com>
2017-01-12 15:30:17 +0100

    all methods added

--- tests-sqlite/tests-sqlite/Program.cs
+++ tests-sqlite/tests-sqlite/Program.cs
@@ -13,24 +13,40 @@ namespace tests_sqlite
         
         static void Main(string[] args)
         {
-
+            //TESTS
             SQLiteDatabase db = new SQLiteDatabase();
             db.LaunchDatabase();
+
             //db.CreateANewUser("oscar", "hehe");
-            if (db.CheckIfPasswordMatch("eustache", "hoho"))
+
+            Dictionary<string, int> gameData = new Dictionary<string, int>();
+            gameData.Add("gameScore", 60);
+            gameData.Add("gameTime", 180);
+            gameData.Add("roundCount", 3);
+            gameData.Add("addCount", 0);
+            gameData.Add("subCount", 50);
+            gameData.Add("multCount", 0);
+            gameData.Add("divCount", 2);
+            db.UpdateData("oscar", gameData);
+
+
+            foreach (KeyValuePair<string, int> data in db.GetData("oscar"))
             {
-                Console.WriteLine("TRUE");
-                Console.ReadKey();
-            }
-            else
-            {
-                Console.WriteLine("FALSE");
-                Console.ReadKey();
+                Console.WriteLine("Key = {0}, Value = {1}", data.Key, data.Value);
             }
             db.CloseDatabase();
+            Console.ReadKey();
         }   
     }
 
+    //LaunchDatabase() - void
+    //CloseDatabase() - void
+    //CreateANewUser(string username, string password) - void
+    //CheckIfUserAlreadyExists(string username) - bool
+    //CheckIfPasswordMatches(string username, string password) - bool
+    //GetData(string username) - dictionary(minScore, maxScore, totalScorePoints, totalGameTimeInSeconds, gameCount, roundCount, addCount, subCount, multCount, divCount)
+    //UpdateData(string username, dictionary(gameScore, gameTime, roundCount, addCount, subCount, multCount, divCount))
+    
     class SQLiteDatabase
     {
         
@@ -44,9 +60,10 @@ namespace tests_sqlite
                 SQLiteConnection.CreateFile("db.sqlite");
             }
             connexion.Open();
-            string sql = "create table if not exists users (username text, password text, minScore int, maxScore int, totalScorePoints int, totalGameTime float, gameCount int, roundCount int, addCount int, subCount int, multCount int, divCount int)";
+            string sql = "create table if not exists users (username text, password text, minScore int, maxScore int, totalScorePoints int, totalGameTimeInSeconds int, gameCount int, roundCount int, addCount int, subCount int, multCount int, divCount int)";
             SQLiteCommand commande = new SQLiteCommand(sql, connexion);
             commande.ExecuteNonQuery();
+            Console.WriteLine("db ouverte / créée avec succès");
             return;
         }
 
@@ -54,13 +71,14 @@ namespace tests_sqlite
         public void CloseDatabase ()
         {
             connexion.Close();
+            Console.WriteLine("db fermée avec succès");
             return;
         }
 
         //Insert un nouvel utilisateur
         public void CreateANewUser (string username, string password)
         {
-            string sql = "insert into users (username, password) values ('" + username + "', '" + password + "')";
+            string sql = "insert into users values ('" + username + "', '" + password + "', 0, 0 ,0 ,0 ,0 ,0, 0, 0, 0, 0)";
             SQLiteCommand commandeInsert = new SQLiteCommand(sql, connexion);
             commandeInsert.ExecuteNonQuery();
             return;
@@ -69,7 +87,7 @@ namespace tests_sqlite
         //Vérifie si l'utilisateur existe déjà - Retourne true si c'est le cas
         public bool CheckIfUserAlreadyExists (string username)
         {
-            string sql = "select username from users where username = '" + username + "'";
+            string sql = "SELECT username FROM users WHERE username = '" + username + "'";
             SQLiteCommand commande = new SQLiteCommand(sql, connexion);
             SQLiteDataReader reader = commande.ExecuteReader();
             while (reader.Read())
@@ -84,7 +102,7 @@ namespace tests_sqlite
         //Vérifie si l'utilisateur existe déjà - Retourne true si c'est le cas
         public bool CheckIfPasswordMatch(string username, string password)
         {
-            string sql = "select password from users where username = '" + username + "'";
+            string sql = "SELECT password FROM users WHERE username = '" + username + "'";
             SQLiteCommand commande = new SQLiteCommand(sql, connexion);
             SQLiteDataReader reader = commande.ExecuteReader();
             while (reader.Read())
@@ -97,6 +115,55 @@ namespace tests_sqlite
             }
             Console.WriteLine("Le mot de passe ne correspond pas");
             return false;
+        }
+
+        //Récupère toutes les données statistiques d'un utilisateur
+        public Dictionary<string, int> GetData(string username)
+        {
+            Dictionary<string, int> userData = new Dictionary<string, int>();
+
+            string sql = "SELECT * FROM users WHERE username = '" + username + "'";
+            SQLiteCommand commande = new SQLiteCommand(sql, connexion);
+            SQLiteDataReader reader = commande.ExecuteReader();
+            while (reader.Read())
+            {
+                //subCount int, multCount int, divCount int)";
+                userData.Add("minScore",reader.GetInt32(2));
+                userData.Add("maxScore", reader.GetInt32(3));
+                userData.Add("totalScorePoints", reader.GetInt32(4));
+                userData.Add("totalGameTimeInSeconds", reader.GetInt32(5));
+                userData.Add("gameCount", reader.GetInt32(6));
+                userData.Add("roundCount", reader.GetInt32(7));
+                userData.Add("addCount", reader.GetInt32(8));
+                userData.Add("subCount", reader.GetInt32(9));
+                userData.Add("multCount", reader.GetInt32(10));
+                userData.Add("divCount", reader.GetInt32(11));
+            }
+            return userData;
+        }
+
+        //Incrémenter la data d'un utilisateur (update) à la fin d'une partie
+        public void UpdateData(string username, Dictionary<string, int> gameData)
+        {
+            string sql = "SELECT minScore, maxScore, gameCount FROM users WHERE username = '" + username + "'";
+            SQLiteCommand commande = new SQLiteCommand(sql, connexion);
+            SQLiteDataReader reader = commande.ExecuteReader();
+            while (reader.Read())
+            {
+                if (reader.GetInt32(2) == 0)
+                {
+                    sql = "UPDATE users SET minScore = " + gameData["gameScore"] + ", maxScore = " + gameData["gameScore"] + ",  totalScorePoints = " + gameData["gameScore"] + ", totalGameTimeInSeconds = " + gameData["gameTime"] + ", gameCount = 1, roundCount = " + gameData["roundCount"] + ", addCount = " + gameData["addCount"] + ", subCount = " + gameData["subCount"] + ", multCount = " + gameData["multCount"] + ", divCount = " + gameData["divCount"] + " WHERE username = '" + username +"'";
+                }
+                else
+                {
+                    int minScore = (reader.GetInt32(0) > gameData["gameScore"] ? gameData["gameScore"] : reader.GetInt32(0));
+                    int maxScore = (reader.GetInt32(1) < gameData["gameScore"] ? gameData["gameScore"] : reader.GetInt32(1));
+                    sql = "UPDATE users SET minScore = " + minScore + ", maxScore = " + maxScore + ", totalScorePoints = totalScorePoints + " + gameData["gameScore"] + ", totalGameTimeInSeconds = totalGameTimeInSeconds + " + gameData["gameTime"] + ", gameCount = gameCount + 1, roundCount = roundCount + " + gameData["roundCount"] + ", addCount = addCount + " + gameData["addCount"] + ", subCount = subCount + " + gameData["subCount"] + ", multCount = multCount + " + gameData["multCount"] + ", divCount = divCount + " + gameData["divCount"] + " WHERE username = '" + username + "'";
+                }
+            }
+            SQLiteCommand commandeInsert = new SQLiteCommand(sql, connexion);
+            commandeInsert.ExecuteNonQuery();
+            return;
         }
     }
 }
