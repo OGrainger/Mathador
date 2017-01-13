5a2036056ce1e013d6c417cebe94a75fb96a8091
clem1302 <clem-lambert@hotmail.fr>
2017-01-13 14:32:19 +0100

    Créa solveur

---
+++ Mathador/Mathador/Solveur.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solveur
+{
+    class Solveur
+    {
+        int i, j, preresult;
+        bool valid;
+        string equation;
+
+        private void simpleSolveur(nb1, nb2)
+        {
+            for (i=1; i<=5; i++)
+            {
+                for (j = 1; j <= 5; j++)
+                {
+                    if (i != j)
+                    {
+                        if (preresult = cible)
+                        {
+
+                        }
+                    }
+                }
+            }
+        }
+        
+        
+        
+         
+    }
+}
