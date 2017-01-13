432b44ee1d19fe4b9d4cc8a8bf0a2ac609e6d995
OGrainger <oscarvgrainger@gmail.com>
2017-01-13 16:58:16 +0100

    simple method for solving 1 op
    in dev - needs to return smth

--- solver-oscar/solver-oscar/Program.cs
+++ solver-oscar/solver-oscar/Program.cs
@@ -12,4 +12,34 @@ namespace solver_oscar
         {
         }
     }
+
+    class Solver
+    {
+        private List<T> ResultsOfOp = new List<T>();
+
+        public void Resolve(int num1, int num2, string operation)
+        {
+            int result;
+            int points;
+            switch (operation)
+            {
+                case "+":
+                    result = num1 + num2;
+                    points = 1;
+                    break;
+                case "-":
+                    result = (num1 - num2 < 0 ? num1 - num2 : -1);
+                    points = 2;
+                    break;
+                case "*":
+                    result = num1 * num2;
+                    points = 1;
+                    break;
+                case "/":
+                    result = (num1 % num2 == 0 ? num1 / num2 : -1);
+                    points = 4;
+                    break;
+            }
+        }
+    }
 }
