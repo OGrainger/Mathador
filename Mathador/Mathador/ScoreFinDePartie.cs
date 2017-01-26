0d1b554e7afe685579ddd7d0e21897b1a52f9d40
OGrainger <oscarvgrainger@gmail.com>
2017-01-26 13:52:57 +0100

    bug update
    bug du retour au menu

--- Mathador/Mathador/ScoreFinDePartie.cs
+++ Mathador/Mathador/ScoreFinDePartie.cs
@@ -12,15 +12,18 @@ namespace Mathador
 {
     public partial class ScoreFinDePartie : Form
     {
+        string pseudo;
+
         public ScoreFinDePartie(string pseudo, int score, int roundCount, int mathadorCount, int plusCount, int moinsCount, int foisCount, int divCount)
         {
             InitializeComponent();
+            this.pseudo = pseudo;
         }
 
         private void buttonMenu_Click(object sender, EventArgs e)
         {
             this.Hide();
-            MenuMathador form = new MenuMathador("eustache");
+            MenuMathador form = new MenuMathador(pseudo);
             form.ShowDialog();
         }
     }
